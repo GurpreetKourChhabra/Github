918484956ede0d1bbf123f8f3302eda595729db6
Gurpreet Chhabra <gurpreetkourchhabra@gmail.com>
2018-06-09 16:49:54 +0530

    Learning commited

--- Practise-Assignments/NodeJs/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'facebookAuth' : {
-        'clientID'      : '145491112763537', // your App ID
-        'clientSecret'  : 'a16f2d8230f42d0446b0a3dd4e01cc81', // your App Secret
-        'callbackURL'   : 'http://localhost:4000/facebook'
-    }
-};
--- Practise-Assignments/NodeJs/fb.js
+++
@@ -1,6 +0,0 @@
-
-//get fb data
-app.get('/fbData' ,function(req, res) {
-
-    res.send(FB + 'fb Data');
-});
